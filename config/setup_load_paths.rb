d98ef2cbff1da8e9e40b16e64631f36b0b99b35d
Alex Pearce <alex@alexpearce.me>
2011-06-06 20:50:00 +0100

    Git test.

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -4,7 +4,7 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
     rvm_lib_path = File.join(rvm_path, 'lib')
     $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
-    #RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
     # RVM is unavailable at this point.
     raise "RVM ruby lib is currently unavailable."
