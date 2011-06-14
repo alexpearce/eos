703535198776a2718b92fef3d194d6f828fcc3c5
Alex Pearce <alex@alexpearce.me>
2011-06-14 13:41:23 +0100

    Modularize that mofo.

--- application.rb
+++ application.rb
@@ -1,28 +1,32 @@
-get '/' do
-  haml :index
-end
+class Selene < Sinatra::Base
 
-# error pages
-not_found do
-  haml :'404'
-end
+  get '/' do
+    haml :index
+  end
 
-error do
-  haml :'500'
-end
+  # error pages
+  not_found do
+    haml :'404'
+  end
 
-helpers do
-  def used_memory
-    return '#' if ENV['RACK_ENV'] == 'development' # OS X doesn't have a free
+  error do
+    haml :'500'
+  end
+
+  helpers do
+    def used_memory
+      return '#' if ENV['RACK_ENV'] == 'development' # OS X doesn't have a free
     
-    `free`.split(" ")[8].to_i / 1000
+      `free`.split(" ")[8].to_i / 1000
+    end
+  
+    def load_average
+      (`uptime`.strip.split(' ').last.to_f * 100).to_i
+    end
+  
+    def uptime
+      `uptime`.strip.split(' ')[2]
+    end
   end
   
-  def load_average
-    (`uptime`.strip.split(' ').last.to_f * 100).to_i
-  end
-  
-  def uptime
-    `uptime`.strip.split(' ')[2]
-  end
 end
--- config.ru
+++ config.ru
@@ -2,14 +2,11 @@ require 'bundler'
 
 Bundler.require
 
-require './application.rb'
-
-set :run, false
-set :environment, :development
+require File.join(File.dirname(__FILE__), 'application.rb')
 
 FileUtils.mkdir_p 'log' unless File.exists?('log')
 log = File.new("log/sinatra.log", "a")
 $stdout.reopen(log)
 $stderr.reopen(log)
 
-run Sinatra::Application
+run Selene
