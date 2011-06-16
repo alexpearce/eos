290cbc1269fbbd67b10666c87d67f8db85770221
Alex Pearce <alex@alexpearce.me>
2011-06-16 15:06:49 +0100

    Use free -m, and use the 'free' memory, i.e. excluding the cache and buffer memory.

--- application.rb
+++ application.rb
@@ -17,7 +17,7 @@ class Selene < Sinatra::Base
     def used_memory
       return '#' if ENV['RACK_ENV'] == 'development' # OS X doesn't have a free
     
-      `free`.split(" ")[8].to_i / 1000
+      `free`.split(" ")[-5]
     end
   
     def load_average
